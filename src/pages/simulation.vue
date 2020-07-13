9553c8bb28e934a25679421cea3100c8b4a46b17
matt_thetechmillion <matt@thetechmillion.com>
2020-07-13 21:35:53 +0200

    Completed load more functionality

--- src/pages/simulation.vue
+++ src/pages/simulation.vue
@@ -2,6 +2,7 @@
   <div>
     <h1 class="text-3xl font-bold text-white my-10">Attacks</h1>
     <grid :attacks="attacks" />
+    <button @click="loadMore">test</button>
   </div>
 </template>
 
@@ -13,12 +14,37 @@ export default {
   components: {
     grid,
   },
+  data() {
+    return {
+      pageCount: 0,
+      canLoadMore: true,
+    };
+  },
   apollo: {
     attacks: {
       query: getAttacks,
       variables: {
         limit: 5,
+        offset: 0,
       },
+    },
+  },
+  methods: {
+    loadMore() {
+      this.$apollo.queries.attacks.fetchMore({
+        variables: {
+          offset: ++this.pageCount * 5,
+          limit: 5,
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          const newAttacks = fetchMoreResult.attacks;
+          this.canLoadMore = newAttacks.length == 5;
+
+          return {
+            attacks: [...previousResult.attacks, ...newAttacks],
+          };
+        },
+      });
     },
   },
 };
